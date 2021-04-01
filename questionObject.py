1d18ee64f15b105a6c9922759d953dc83db16d18
Mimmi Matalamäki <mimmi.matalamaki@tuni.fi>
2021-04-01 09:54:02 +0300

    questionObject class created

---
+++ questionObject.py
@@ -0,0 +1,38 @@
+# Class to hold information for one question
+class questionObject:
+
+    def __init__(self, questionNumber, question, answerOptions, rightAnswer):
+        self.__questionNumber = questionNumber
+        self.__question = question
+        self.__answerOptions = answerOptions
+        self.__rightAnswer = rightAnswer
+
+    def setQuestionNumber(self, questionNumber):
+        if isinstance(questionNumber, int):
+            self.__questionNumber = questionNumber
+
+    def getQuestionNumber(self):
+        return self.__questionNumber
+
+    def setQuestion(self, question):
+        if isinstance(question, str):
+            self.__question = question
+
+    def getQuestion(self):
+        return self.__question
+
+    def setAnswerOptions(self, answerOptions):
+        if isinstance(answerOptions, dict):
+            self.__answerOptions = answerOptions
+
+    def getAnswerOptions(self):
+        return self.__AnswerOptions
+
+    def setRightAnswer(self, rightAnswer):
+        if isinstance(rightAnswer, str):
+            self.__rightAnswer = rightAnswer
+
+    def getRightAnswer(self):
+        return self.__rightAnswer
+
+
