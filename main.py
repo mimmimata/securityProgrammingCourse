305705642ced695d4d6364eae37f9d5f4d34e3fd
Mimmi Matalamäki <mimmi.matalamaki@tuni.fi>
2021-04-23 13:22:02 +0300

    valid player amount is asked many times if wrong

--- main.py
+++ main.py
@@ -6,26 +6,31 @@ from validator import *
 def main():
     playersList = []
 
-    try:
-        amountOfPlayersString = str(input("Enter amount of wanted players (2-4): "))
-    except ValueError:
-        print("User input cannot be casted as a string, something is horrible wrong!")
-        return
-    # User input must be validate to be in accepted range of player amount
-    if isValidPlayerAmount(amountOfPlayersString):
-        amountOfPlayers = int(amountOfPlayersString)
+    amountOfPlayers = 0
+    while True:
+        try:
+            amountOfPlayersString = str(input("Enter amount of wanted players (2-4): "))
+        except ValueError:
+            print("User input cannot be casted as a string, something is horrible wrong!")
+            return
 
-        playerNumber = 1
-        i = 0
-        while i < amountOfPlayers:
-            playername = input("Enter username of {}. player: ".format(playerNumber))
-            if not isValidPlayerName(playername):
-                return
-            player = Player(playerNumber, playername)
-            playersList.append(player)
-            playerNumber = playerNumber + 1
-            i = i + 1
+        # User input must be validate to be in accepted range of player amount
+        if isValidPlayerAmount(amountOfPlayersString):
+            amountOfPlayers = int(amountOfPlayersString)
+            break
+        else:
+            print("Invalid number of players, player amount needs to be between 2 and 4.")
+    playerNumber = 1
+    i = 0
+    while i < amountOfPlayers:
+        playername = input("Enter username of {}. player: ".format(playerNumber))
+        if not isValidPlayerName(playername):
+            return
+        player = Player(playerNumber, playername)
+        playersList.append(player)
+        playerNumber = playerNumber + 1
+        i = i + 1
+    game = Game(amountOfPlayers, playersList)
+    game.startGame()
 
-        game = Game(amountOfPlayers, playersList)
-        game.startGame()
 main()
